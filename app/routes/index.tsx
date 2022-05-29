ea2a2203ce5dddbcf33fdc751fe1b1cf9b75284c
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-05-28 23:00:42 -0500

    index displays todo item

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,24 @@
-export default function Index() {
+import type { LoaderFunction } from '@remix-run/node';
+import { supabase } from '../../libs/supabase-client';
+import { useLoaderData } from '@remix-run/react';
+
+export const loader: LoaderFunction = async () => {
+  const { data } = await supabase.from("todos").select("*");
+  return data;
+};
+
+
+const Index = () => {
+  const data = useLoaderData();
+  console.log('DATA?', data)
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <div>
+      <h1>Hello world</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {data.map(o => <li>{o.item}</li>)}
       </ul>
     </div>
   );
 }
+
+export default Index;
